099dccd5364f685a1b4e89749ed32d6831a50fb2
Nacho Cabanes <nacho@nachocabanes.com>
2019-09-18 10:56:40 +0200

    Monogame 01c - Spaceship image; resized to 960x600

--- chapter01-contactWithMonogame/projectInUse/Monogame01/Monogame01/Game1.cs
+++ chapter01-contactWithMonogame/projectInUse/Monogame01/Monogame01/Game1.cs
@@ -4,20 +4,22 @@ using Microsoft.Xna.Framework.Input;
 
 namespace Monogame01
 {
-    /// <summary>
-    /// This is the main type for your game.
-    /// </summary>
     public class Game1 : Game
     {
         GraphicsDeviceManager graphics;
         SpriteBatch spriteBatch;
 
-        Texture2D background;
+        Texture2D background, spaceship;
 
         public Game1()
         {
             graphics = new GraphicsDeviceManager(this);
             Content.RootDirectory = "Content";
+
+            // Screen size: 960x600
+            graphics.PreferredBackBufferWidth = 960;
+            graphics.PreferredBackBufferHeight = 600;
+            graphics.ApplyChanges();
         }
 
         protected override void Initialize()
@@ -28,6 +30,7 @@ namespace Monogame01
         protected override void LoadContent()
         {
             background = Content.Load<Texture2D>("fondo960");
+            spaceship = Content.Load<Texture2D>("nave");
             spriteBatch = new SpriteBatch(GraphicsDevice);
         }
 
@@ -48,6 +51,9 @@ namespace Monogame01
             spriteBatch.Begin();
             spriteBatch.Draw(background, 
                 new Rectangle(0, 0, 960, 600), 
+                Color.White);
+            spriteBatch.Draw(spaceship,
+                new Rectangle(470, 500, spaceship.Width, spaceship.Height),
                 Color.White);
             spriteBatch.End();
 
