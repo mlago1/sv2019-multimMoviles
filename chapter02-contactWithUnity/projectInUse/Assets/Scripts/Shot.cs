ea1e73247994cdd886623c462c805aea6e14212c
Nacho Cabanes <nacho@nachocabanes.com>
2019-09-25 23:25:25 +0200

    Space Hawks 007 (Unity) - Shot

---
+++ chapter02-contactWithUnity/projectInUse/Assets/Scripts/Shot.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shot : MonoBehaviour
+{
+    [SerializeField] float shotSpeed = 100;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        gameObject.GetComponent<Rigidbody2D>().
+            velocity = new Vector2(0, shotSpeed * Time.deltaTime);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (transform.position.y > 5)
+            Destroy(gameObject);
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag == "Enemy")
+        {
+            Destroy(other.gameObject);
+            Destroy(gameObject);
+        }
+    }
+}
