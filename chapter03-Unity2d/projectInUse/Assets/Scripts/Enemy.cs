3d5d1de8cb32653091e9a922af097edfb39dda89
Nacho Cabanes <nacho@nachocabanes.com>
2019-09-26 19:31:33 +0200

    Space Hawks 009 (Unity) - Enemy shots (in use)

--- chapter03-Unity2d/projectInUse/Assets/Scripts/Enemy.cs
+++ chapter03-Unity2d/projectInUse/Assets/Scripts/Enemy.cs
@@ -6,7 +6,13 @@ public class Enemy : MonoBehaviour
 {
     private float speedX = 3;
     private float speedY = 3;
+    private float shotSpeed = 100;
+    [SerializeField] Transform shotPrefab;
 
+    private void Start()
+    {
+        StartCoroutine( Shoot() );
+    }
 
     void Update()
     {
@@ -15,5 +21,17 @@ public class Enemy : MonoBehaviour
         if (transform.position.y < -3 || transform.position.y > 3)
             speedY = -speedY;
         transform.Translate(speedX * Time.deltaTime, speedY * Time.deltaTime, 0);
+    }
+
+    IEnumerator Shoot()
+    {
+        float pause = Random.Range(3.0f, 8.0f);
+        yield return new WaitForSeconds(pause);
+        Transform shot = Instantiate(shotPrefab, 
+            transform.position, Quaternion.identity);
+        shot.gameObject.GetComponent<Rigidbody2D>().velocity = 
+            new Vector3(0, -shotSpeed*Time.deltaTime, 0);
+        Destroy(shot.gameObject, 3);
+        StartCoroutine(Shoot());
     }
 }
